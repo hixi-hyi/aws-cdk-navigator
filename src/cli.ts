7855d0c6401fdd4077d6cab18b258c125997b00c
Hiroyoshi Houchi <git@hixi-hyi.com>
2024-01-16 03:17:29 +0900

    refactoring

--- src/cli.ts
+++ src/cli.ts
@@ -1,40 +1,42 @@
-import Table from 'cli-table3';
-import yesno from 'yesno';
+import * as prompts from '@inquirer/prompts';
+import { table } from 'table';
 import { Context } from './context';
-import spawn from 'child_process';
+import { spawn } from 'child_process';
 
 interface CliOptions {
   argv: string[];
   accountId: string;
   accountAlias: string;
 }
-class Cli {
+export class Cli {
   public readonly argv: string[];
   public readonly command: string;
   public readonly accountId: Context;
   public readonly accountAlias: Context;
-  public readonly table: Table;
+  public readonly accountInfo: string;
 
   public constructor(opts: CliOptions) {
     this.argv = opts.argv;
     this.command = this.argv[0] || '';
     this.accountId = new Context(opts.accountId);
     this.accountAlias = new Context(opts.accountAlias);
-    this.table = new Table({ head: ['accountId', 'accountAlias'], style: { head: [], border: [] } });
-    this.table.push([this.accountId.value, this.accountAlias.value]);
+    this.accountInfo = table([
+      ['accountId', 'accountAlias'],
+      [opts.accountId, opts.accountAlias],
+    ]);
   }
 
   public printAwsAccountInfo(): void {
-    console.log(jthis.table.toString());
+    console.log(this.accountInfo);
   }
 
   public async confirm(): Promise<boolean> {
-    const ok = await yesno({
-      question: `You will need to confirm to operate on this account.
-${this.table.toString()}
+    const ok = await prompts.confirm({
+      message: `You will need to confirm to operate on this account.
+${this.accountInfo}
 Please confirm account infomation before executing.
 Are you sure you want to continue? ([y]es/[n]o)`,
-      defaultValue: 'n',
+      default: false,
     });
     return ok;
   }
